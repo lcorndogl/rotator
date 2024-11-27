2e677292b4a83ffc04c87a0eaeb470e40ece95a0
Michael Cornall <mik.reds.forever@gmail.com>
2024-11-27 20:46:31 +0000

    Add timer variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
+// Returns a Promise that resolves after "ms" Milliseconds
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
 async function rotate(image) {
     for (let i = 0; i <= 3600; i++) {
         image.setAttribute("style", "transform: rotate(" + i + "deg)");
-        await timer(2000);
+        await timer(20);
     }
 }
